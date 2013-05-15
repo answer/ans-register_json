cbb6ee2a77cf55de3e03663583d9af912a62b575
sakai shunsuke <sakai@ans-web.co.jp>
2013-05-15 11:34:36 +0900

    ヘルパーメソッドの追加

--- lib/ans-register_json.rb
+++ lib/ans-register_json.rb
@@ -2,6 +2,40 @@ require "ans-register_json/version"
 
 module Ans
   module RegisterJson
-    # Your code goes here...
+    class Config
+      def initialize
+        @data_method = "data"
+        @dump_method = "dump_data"
+        @json_data_name = "data"
+      end
+
+      attr_accessor :data_method, :dump_method, :json_data_name
+    end
+
+    def self.config
+      @config ||= Config.new
+    end
+    def self.configure
+      yield self.config
+    end
+
+    module Controller
+      def self.included(m)
+        config = RegisterJson.config
+
+        define_method config.data_method.to_sym do
+          @ans_register_json_data ||= {}
+        end
+      end
+    end
+    module View
+      def self.included(m)
+        config = RegisterJson.config
+
+        define_method config.dump_method.to_sym do
+          raw '<script type="text/javascript">window.'+config.json_data_name+' = '+@ans_register_json_data.to_json+'</script>'
+        end
+      end
+    end
   end
 end
